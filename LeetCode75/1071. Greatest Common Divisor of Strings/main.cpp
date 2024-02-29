500451eb7478bc7680303d2e0e298325ddfe8fc2
superc4be <gimazetdinovdmitry@google.com>
2024-02-29 12:48:55 +0300

    1071

---
+++ LeetCode75/1071. Greatest Common Divisor of Strings/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::string, std::cin, std::cout;
+
+
+string gcdOfStrings(string str1, string str2) {
+    if (str1.size() < str2.size()) {
+        return gcdOfStrings(str2, str1);
+    }
+
+    if (str1.find(str2) != 0) {
+        return "";
+    }
+
+    if (str2 == "") {
+        return str1;
+    }
+
+    return gcdOfStrings(str1.substr(str2.length()), str2);
+}
+
+int main() {
+    
+    return 0;
+}
